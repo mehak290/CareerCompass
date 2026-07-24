7d4c621d9e359d94b70597023833327bdf8bd4b8
Mehak Sampla <mehaksampla10@gmail.com>
2026-07-24 19:59:32 +0530

    Prepare Career Compass v1 for deployment

--- src/main/java/careercompass/CareercompassApplication.java
+++ src/main/java/careercompass/CareercompassApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CareercompassApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CareercompassApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CareercompassApplication.class, args);
+    }
 
 }
